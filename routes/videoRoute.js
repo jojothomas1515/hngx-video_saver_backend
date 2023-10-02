6c07afc9facf6344518afe95c1d5272adcff4498
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-02 18:30:11 +0100

    wahala wahala wahala

--- routes/videoRoute.js
+++ routes/videoRoute.js
@@ -1,11 +1,7 @@
 const { Router } = require('express');
 const { uploadVideo, endUpload, startRecord, getAllVideos, getVid } = require('../controllers/videoController');
-const fileupload = require('express-fileupload');
 const { streamVideo } = require('../controllers/streamVideoController');
-const multer = require('multer');
 const vidRoutes = Router();
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
 
 // Video recording
 vidRoutes.post('/start', startRecord);
